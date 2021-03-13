401934621de89fe40796da12f2f723c20b5326e9
QandeelAsif <qandeel.asif@gmail.com>
2021-03-13 12:14:03 +0500

    pointer points at my co-ords. Is not updating

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, PermissionsAndroid, Platform } from 'react-native'
+import MapScreen from './MapScreen';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hasMapPermission: false
+    }
+  }v
+
+  componentDidMount(){
+    this.requestFineLocation();
+    
+  }
+
+  async requestFineLocation(){
+    try {
+      if(Platform.OS === 'android'){
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+        if(granted === PermissionsAndroid.RESULTS.GRANTED){
+          this.setState({hasMapPermission: true})
+        }
+      }
+      else {
+        this.setState({hasMapPermission:true})
+      } 
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  render() {
+    if (this.state.hasMapPermission){
+      return <MapScreen />;
+    }
+    return null;
+    
+  }
+}
+
+const styles = StyleSheet.create({})
---
+++ README.md
@@ -0,0 +1,31 @@
+# React Native Trackee App
+
+> A Realtime location tracking app for [article](https://www.sitepoint.com/react-native-pubnub-real-time-location-tracking/) published on SitePoint. This app is one of the two location tracking app. This app will be tracked by the [Tracker](https://github.com/vikrantnegi/tracker-app) app.
+
+
+# Clone the repo
+
+```bash
+$ git clone https://github.com/vikrantnegi/trackee-app.git
+```
+
+# Install the dependencies
+
+```bash
+$ yarn
+```
+
+## Get PubNub keys
+
+Log in to your [PubNub](https://dashboard.pubnub.com/login) account and go to the dashboard. Find `Publish Key` and `Subscribe Key` and add them in the [`App.js`](https://github.com/vikrantnegi/trackee-app/blob/master/App.js#L31-L32)
+
+# Run the app
+
+```bash
+$ react-native run-ios
+$ react-native run-android
+```
+
+# License
+
+MIT © [Vikrant Negi](https://github.com/vikrantnegi)
